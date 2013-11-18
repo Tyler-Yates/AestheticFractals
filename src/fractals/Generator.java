fe647521bac031c5371796b5b2d73d693286f76c
Tyler <tyler.yates@utexas.edu>
2013-11-18 12:01:54 -0800

    Multi-threading

--- src/fractals/Generator.java
+++ src/fractals/Generator.java
@@ -3,77 +3,64 @@ package fractals;
 import graphics.GraphicalInterface;
 
 import java.awt.Graphics;
-import java.io.IOException;
 import java.util.ArrayList;
 
-public class Generator
-{
-    int generation = 0; //The current generation's number
-    
-    static ArrayList<Fractal> fractals = new ArrayList<Fractal>(9);//Current Fractals
-    static ArrayList<Fractal> selectedFractals = new ArrayList<Fractal>(9);
-    
-    static double crossRatio, mutateRatio, cloneRatio;
-    
-    static {
-        crossRatio = 0.3;
-    }
-    
-    public static void generateNewGeneration()
-    {
-        for(int i=0; i<GraphicalInterface.selectedFractals.length; i++)
-        {
-            if(GraphicalInterface.selectedFractals[i])
-                selectedFractals.add(fractals.get(i));
-        }
-        
-        fractals.clear();
-        
-        Fractal newFractal;
-        for(int i=0; i<9; i++)
-        {
-            if (selectedFractals.isEmpty()) {
-                newFractal = new Fractal();
-            }
-            else if (selectedFractals.size() == 1) {
-                newFractal = selectedFractals.get(0).mutate();
-            } else {
-                Fractal Parent1 = selectedFractals.get((int)(Math.random()*selectedFractals.size()));
-                Fractal Parent2 = selectedFractals.get((int)(Math.random()*selectedFractals.size()));
-            
-                if(i<=9*crossRatio) {
-                    System.out.println("crossing i=" + i);
-                    newFractal = Parent1.cross(Parent2);
-                }
-                else {
-                //else if (i<=9*mutateRatio) {
-                    System.out.println("mutating i=" + i);
-                    newFractal = Parent1.mutate();
-                }
-                /*else {
-                    System.out.println("cloning i=" + i);
-                    newFractal = Parent1.clone();
-                }*/
-            }
-            
-            try
-            {
-                newFractal.generateImage();
-            } catch (IOException | InterruptedException e)
-            {
-                System.out.println("error");
-                
-                e.printStackTrace();
-            }
-            fractals.add(newFractal);
-        }
-        
-        selectedFractals.clear();
-    }
+public class Generator {
+	int generation = 0; // The current generation's number
 
-    public static void drawImage(int index, Graphics g, int x, int y)
-    {
-        fractals.get(index).drawImage(g, x, y);
-        
-    }
+	static ArrayList<Fractal> fractals = new ArrayList<Fractal>(9);// Current
+																	// Fractals
+	static ArrayList<Fractal> selectedFractals = new ArrayList<Fractal>(9);
+
+	static double crossRatio, mutateRatio, cloneRatio;
+
+	static {
+		crossRatio = 0.3;
+	}
+
+	public static void generateNewGeneration() {
+		for (int i = 0; i < GraphicalInterface.selectedFractals.length; i++) {
+			if (GraphicalInterface.selectedFractals[i])
+				selectedFractals.add(fractals.get(i));
+		}
+
+		fractals.clear();
+
+		Fractal newFractal;
+		for (int i = 0; i < 9; i++) {
+			if (selectedFractals.isEmpty()) {
+				newFractal = new Fractal();
+			} else if (selectedFractals.size() == 1) {
+				newFractal = selectedFractals.get(0).mutate();
+			} else {
+				Fractal Parent1 = selectedFractals
+						.get((int) (Math.random() * selectedFractals.size()));
+				Fractal Parent2 = selectedFractals
+						.get((int) (Math.random() * selectedFractals.size()));
+
+				if (i <= 9 * crossRatio) {
+					System.out.println("crossing i=" + i);
+					newFractal = Parent1.cross(Parent2);
+				} else {
+					// else if (i<=9*mutateRatio) {
+					System.out.println("mutating i=" + i);
+					newFractal = Parent1.mutate();
+				}
+				/*
+				 * else { System.out.println("cloning i=" + i); newFractal =
+				 * Parent1.clone(); }
+				 */
+			}
+			fractals.add(newFractal);
+		}
+
+		ImageManager.generateNewImages(fractals);
+
+		selectedFractals.clear();
+	}
+
+	public static void drawImage(int index, Graphics g, int x, int y) {
+		fractals.get(index).drawImage(g, x, y);
+
+	}
 }
--- src/graphics/GraphicalInterface.java
+++ src/graphics/GraphicalInterface.java
@@ -14,200 +14,182 @@ import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 import fractals.Generator;
+import fractals.ImageManager;
 
 /**
  * Opens JFrame with nine boxes.
  * 
  */
 public class GraphicalInterface extends JPanel implements MouseMotionListener,
-        MouseListener, KeyListener
-{
-    private static final long serialVersionUID = 749344840243728058L;
+		MouseListener, KeyListener {
+	private static final long serialVersionUID = 749344840243728058L;
 
-    public static JFrame frame;
-    public static JLayeredPane selector;
-    
-    public static boolean selectedFractals[] = new boolean[9];
+	public static JFrame frame;
+	public static JLayeredPane selector;
 
-    static int windowWidth = 1024, windowHeight = 768;
-    static int mouseX, mouseY; // Mouse location on the screen
+	public static boolean selectedFractals[] = new boolean[9];
 
-    private static final double VERSION = 0.1;
-    
-    private static int selectedBoxX,selectedBoxY;
+	static int windowWidth = 1024, windowHeight = 768;
+	static int mouseX, mouseY; // Mouse location on the screen
 
-    /**
-     * Initializes the JFrame
-     */
-    public GraphicalInterface()
-    {
-        frame = new JFrame("Aesthetic Fractal v" + VERSION);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(windowWidth, windowHeight);
-        frame.addMouseMotionListener(this);
-        frame.addMouseListener(this);
-        frame.addKeyListener(this);
-        frame.add(this);
-    }
+	private static final double VERSION = 0.1;
 
-    public static void main(String args[]) throws IOException, InterruptedException
-    {  
-        Generator.generateNewGeneration();
-        new GraphicalInterface();
-    }
+	private static int selectedBoxX, selectedBoxY;
 
-    public void drawInterface(Graphics g)
-    {
-        g.setColor(Color.white);
-        g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
-        
-        int boxWidth = frame.getWidth() / 3;
-        int boxHeight = frame.getHeight() / 3;
+	/**
+	 * Initializes the JFrame
+	 */
+	public GraphicalInterface() {
+		frame = new JFrame("Aesthetic Fractal v" + VERSION);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(windowWidth, windowHeight);
+		frame.addMouseMotionListener(this);
+		frame.addMouseListener(this);
+		frame.addKeyListener(this);
+		frame.add(this);
+	}
 
-        for (int i = 0; i < 9; i++)
-        {
-            int x = i % 3 * boxWidth;
-            int y = i / 3 * boxHeight;
+	public static void main(String args[]) throws IOException,
+			InterruptedException {
+		Generator.generateNewGeneration();
+		new GraphicalInterface();
+	}
 
-            
-            drawImage(g, i, x, y);
-            drawBox(g, x, y, boxWidth, boxHeight);
-            drawSelectedBoxes(g);
-        }
-    }
+	public void drawInterface(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
 
-    private void drawSelectedBoxes(Graphics g)
-    { 
-        g.setColor(new Color(0, 255, 0, 15));
-        for(int i=0; i<selectedFractals.length; i++)
-        {
-            int x=i%3*frame.getWidth()/3;
-            int y=i/3*frame.getHeight()/3;
-            
-            if(selectedFractals[i])
-            {
-                g.fillRect(x, y, frame.getWidth()/3, frame.getHeight()/3);
-            }
-        }
-    }
+		int boxWidth = frame.getWidth() / 3;
+		int boxHeight = frame.getHeight() / 3;
 
-    public void drawBox(Graphics g, int x, int y, int width, int height)
-    {
-        g.setColor(Color.black);
-        g.drawRect(x, y, width, height);
-    }
-    
-    public void drawImage(Graphics g, int index, int x, int y)
-    {
-        Generator.drawImage(index, g, x, y);
-    }
+		for (int i = 0; i < 9; i++) {
+			int x = i % 3 * boxWidth;
+			int y = i / 3 * boxHeight;
 
-    public void drawSelectedBox(Graphics g)
-    {
-        int l = mouseX / (frame.getWidth() / 3) * (frame.getWidth() / 3);
-        int t = mouseY / (frame.getHeight() / 3) * (frame.getHeight() / 3);
+			g.setColor(Color.black);
+			if(ImageManager.indexIsReady(i))
+				drawImage(g, i, x, y);
+			else
+				g.drawString("Loading...", x+20, y+20);
+			drawBox(g, x, y, boxWidth, boxHeight);
+			drawSelectedBoxes(g);
+		}
+	}
 
-        g.setColor(new Color(0, 0, 255, 15));
-        g.fillRect(l + 1, t + 1, frame.getWidth() / 3 - 2,
-                frame.getHeight() / 3 - 2);
-    }
+	private void drawSelectedBoxes(Graphics g) {
+		g.setColor(new Color(0, 255, 0, 15));
+		for (int i = 0; i < selectedFractals.length; i++) {
+			int x = i % 3 * frame.getWidth() / 3;
+			int y = i / 3 * frame.getHeight() / 3;
 
-    public void paint(Graphics g) {
-            drawInterface(g);
-            drawSelectedBox(g);	
-    }
+			if (selectedFractals[i]) {
+				g.fillRect(x, y, frame.getWidth() / 3, frame.getHeight() / 3);
+			}
+		}
+	}
 
-    @Override
-    public void mouseDragged(MouseEvent e)
-    {
+	public void drawBox(Graphics g, int x, int y, int width, int height) {
+		g.setColor(Color.black);
+		g.drawRect(x, y, width, height);
+	}
 
-    }
+	public void drawImage(Graphics g, int index, int x, int y) {
+		Generator.drawImage(index, g, x, y);
+	}
 
-    @Override
-    public void mouseMoved(MouseEvent e)
-    {
-        int x = e.getX();
-        int y = e.getY();
+	public void drawSelectedBox(Graphics g) {
+		int l = mouseX / (frame.getWidth() / 3) * (frame.getWidth() / 3);
+		int t = mouseY / (frame.getHeight() / 3) * (frame.getHeight() / 3);
 
-        mouseX = x - frame.getInsets().left;
-        mouseY = y - frame.getInsets().top;
-        
-        int l = mouseX / (frame.getWidth() / 3) * (frame.getWidth() / 3);
-        int t = mouseY / (frame.getHeight() / 3) * (frame.getHeight() / 3);
-        
-        if(l!=selectedBoxX | t!=selectedBoxY)
-        {
-            selectedBoxX=l;
-            selectedBoxY=t;
-            repaint();
-        }
-    }
+		g.setColor(new Color(0, 0, 255, 15));
+		g.fillRect(l + 1, t + 1, frame.getWidth() / 3 - 2,
+				frame.getHeight() / 3 - 2);
+	}
 
-    @Override
-    public void mouseClicked(MouseEvent e)
-    {
-        int x = e.getX();
-        int y = e.getY();
-        
-        mouseX = x - frame.getInsets().left;
-        mouseY = y - frame.getInsets().top;
-        
-        int boxCol = mouseX / (frame.getWidth() / 3);
-        int boxRow = mouseY / (frame.getHeight() / 3);
-        
-        int boxIndex = (boxRow*3) + boxCol;
-        selectedFractals[boxIndex]=!selectedFractals[boxIndex];
-        repaint();
-    }
+	public void paint(Graphics g) {
+		drawInterface(g);
+		drawSelectedBox(g);
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e)
-    {
+	@Override
+	public void mouseDragged(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseReleased(MouseEvent e)
-    {
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
 
-    }
+		mouseX = x - frame.getInsets().left;
+		mouseY = y - frame.getInsets().top;
 
-    @Override
-    public void mouseEntered(MouseEvent e)
-    {
+		int l = mouseX / (frame.getWidth() / 3) * (frame.getWidth() / 3);
+		int t = mouseY / (frame.getHeight() / 3) * (frame.getHeight() / 3);
 
-    }
+		if (l != selectedBoxX | t != selectedBoxY) {
+			selectedBoxX = l;
+			selectedBoxY = t;
+			repaint();
+		}
+	}
 
-    @Override
-    public void mouseExited(MouseEvent e)
-    {
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
 
-    }
+		mouseX = x - frame.getInsets().left;
+		mouseY = y - frame.getInsets().top;
 
-    @Override
-    public void keyPressed(KeyEvent e)
-    {
-        // TODO Auto-generated method stub
-        
-    }
+		int boxCol = mouseX / (frame.getWidth() / 3);
+		int boxRow = mouseY / (frame.getHeight() / 3);
 
-    @Override
-    public void keyReleased(KeyEvent e)
-    {
-        if(e.getKeyCode()==KeyEvent.VK_SPACE)
-        {
-            Generator.generateNewGeneration();
-            selectedFractals = new boolean[9];
-            repaint();
-        }
-        
-    }
+		int boxIndex = (boxRow * 3) + boxCol;
+		selectedFractals[boxIndex] = !selectedFractals[boxIndex];
+		repaint();
+	}
 
-    @Override
-    public void keyTyped(KeyEvent e)
-    {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			Generator.generateNewGeneration();
+			selectedFractals = new boolean[9];
+			repaint();
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
